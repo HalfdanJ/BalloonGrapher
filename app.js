ed2f6b9665a7fb41e0328adfb97dcbaf78f1794c
Jonas Jongejan <jonas@halfdanj.dk>
2015-08-21 13:25:06 +0200

    redis

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
+
 var express = require('express');
+var redis = require('redis');
+var url = require('url');
+
+var redisURL = url.parse(process.env.REDIS_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname);
+if(redisURL.auth) {
+  client.auth(redisURL.auth.split(":")[1]);
+}
+
+
+client.get('startup', function(err,d){
+  console.log("Last startup: ",d);
+})
+client.set('startup',new Date());
+
+
 var app = express();
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,6 +30,15 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('index.html');
 });
+
+app.get('/startup', function(request, response) {
+
+  client.get('startup', function(err,d){
+    response.send(d);
+  })
+
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
