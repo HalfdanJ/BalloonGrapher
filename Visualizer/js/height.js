348c8662728d351395b334b4ebf33b7dc0afdba4
Jonas Jongejan <jonas@halfdanj.dk>
2015-08-24 12:49:13 +0200

    update

--- Visualizer/js/height.js
+++ Visualizer/js/height.js
@@ -13,8 +13,11 @@ var DK = d3.locale( {
   "shortMonths": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 });
 
+var startTime = moment('15:33', 'HH:mm');
+var endTime = startTime.clone().add(3,'hours');
+
 var chart = {
-  data: null,
+  heightData: null,
   simulationData: null,
 
   xScale: null,
@@ -25,20 +28,17 @@ var chart = {
   lineWidth: 600,
   lineHeight: 150,
 
+  livePath: null,
+  valueLine:null,
+
+
   init: function() {
     var data = {};
 
-    var	dataset = _.map(this.data.height, function(data, k) {
-      /*      var yearAve = _.reduce(data, function(m, v) {return m + v;}, 0) / 12;
-       var yearMax = _.max(data);
-       var yearMin = _.min(data);*/
-      return {/*year: +k, */data: data/*, mean: yearAve, max: yearMax, min: yearMin*/};
-    });
-    this.data.height = {data: dataset.reverse() , extent: [-2, 16]};
 
     this.simulationData = _.values(_.groupBy(this.simulationData, 'simulation'));
 
-    console.log(this.simulationData);
+    console.log(this.heightData);
 
     this.initChart();
   },
@@ -50,31 +50,31 @@ var chart = {
       width = $(window).width() - margin.left - margin.right,
       height = $(window).height() - margin.top - margin.bottom;
 
-    var x = d3.time.scale()
+    this.x = d3.time.scale()
       .domain([
-        this.simulationData['1'][0].time,
-        _.max(_.pluck(this.simulationData['4'],'time'))
-        ])
+        startTime.toDate(),
+        endTime.toDate()
+      ])
       .range([0, width]);
 
-    var y = d3.scale.linear()
+    this.y = d3.scale.linear()
       .domain([
-        0,_.max(_.pluck(this.simulationData['1'],'altitude'))+5000,
+        0,40000,
       ])
       .range([height, 0]);
 
     // Define the axes
-    var xAxis = d3.svg.axis().scale(x)
+    var xAxis = d3.svg.axis().scale(this.x)
       .orient("bottom")
       .ticks(3)
       .innerTickSize(-height)
-      .tickFormat(d3.time.format("%H:%M"));
+      .tickFormat(function(d){ return d3.time.format("%H:%M")(d)} );
 
-    var yAxis = d3.svg.axis().scale(y)
+    var yAxis = d3.svg.axis().scale(this.y)
       .orient("left")
       .ticks(5)
       .innerTickSize(-width)
-      .tickFormat(DK.numberFormat(',m'))
+      .tickFormat(function(d){ return DK.numberFormat(',m')(d)+' m'})
 
 
     var area = d3.svg.area()
@@ -83,9 +83,10 @@ var chart = {
       .y1(function(d) { return y(d.altitude); });
 
     // Define the line
-    var valueline = d3.svg.line()
-      .x(function(d) { return x(d.time); })
-      .y(function(d) { return y(d.altitude); });
+    this.valueline = d3.svg.line()
+      .interpolate("basis")
+      .x(function(d) { return that.x(d.time); })
+      .y(function(d) { return that.y(d.altitude); });
 
 
     var svg = d3.select("body")
@@ -93,48 +94,51 @@ var chart = {
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
-      .classed('simulation', true)
       .attr("transform","translate(" + margin.left + "," + margin.top + ")")
 
-    var simulation = svg
-      .selectAll('g.simulation')
 
+
+
+    var simulation = svg
+      .append("g")
+      .classed('simulation', true)
+      .selectAll('g.simulation')
       .data(this.simulationData)
       .enter()
       .append('g')
-    /*
-     simulation.append('path')
+
+
+    this.livePath = svg
+      .append("g")
+      .classed('live', true)
+      .append('path')
+
+    this.liveHeight = svg
+      .append('path')
+      .classed('line liveheight',true)
+
+    this.liveHeightText = svg
+      .append('text')
+
+
+    /* .selectAll('g.live')
+     .attr("transform","translate(" + margin.left + "," + margin.top + ")")
+     .data([this.heightData])
+     .enter()
+     .append('g')*/
+
+
+
+    /*simulation.append('path')
      .attr('class', 'area')
      .attr('d', area)*/
 
     simulation.append('path')
       .attr("class", "line")
-      .style("stroke-dasharray", ("3, 3"))
-      .attr("d", valueline);
-
-    //.attr('transform', this.translate(30, this.chartHeight))
-    /* .selectAll('g.year')
-     .data(this.data.height.data)
-     .enter()
-     .append('g')
-     .attr('class', function(d, i) {
-     console.log(d);
-     'year-' + d.year;})*/
-    /*.classed('year', true)
-     //.sort(this.sortFunction[this.uiState.sortBy])
-     .attr('transform', function(d, i) {
-     //return that.translate(i * that.perspectiveOffsetX, -i * that.perspectiveOffsetY);
-     })
-     .style('opacity', function(d, i) {
-     //  return that.colorScale(i);
-     });*/
+      //.style("stroke-dasharray", ("5, 10"))
+      .attr("d", this.valueline);
 
 
-    // Add the valueline path.
-    /* svg.append("path")
-     .attr("class", "line")
-     .attr("d", valueline(this.simulationData['1']));
-     */
     // Add the X Axis
     svg.append("g")
       .attr("class", "x axis")
@@ -145,29 +149,61 @@ var chart = {
     svg.append("g")
       .attr("class", "y axis")
       .call(yAxis);
+  },
+
+  updateHeight:function(data){
+    this.heightData = data;
+
+    this.livePath
+      .data([this.heightData])
+      .attr("class", "line")
+      .attr("d", this.valueline);
+
+    var last = _.last(this.heightData);
+    var altitude = last.altitude;
+    this.liveHeight
+      .transition()
+      .attr('d', this.valueline([{
+        time:startTime.toDate(), altitude:altitude
+      }, {
+        time:endTime.toDate(), altitude:altitude
+      }, ]))
+
+    this.liveHeightText
+      .text(DK.numberFormat(',')(Math.round(altitude))+" m")
+      .attr('x', 10)
+      .attr('y', this.y(altitude)-5);
+
+
+
   }
 }
 
-d3.json('data/height.json', function(heightData) {
 
-  chart.data = heightData;
+d3.csv('data/ASTRA Simulation Results.csv')
+  .row(function(d){
 
-  d3.csv('data/ASTRA Simulation Results.csv')
-    .row(function(d){
-      var startTime = moment('18:40', 'HH:mm');
-      var t = startTime.clone().add(parseInt(d['Time from launch [s]']),'seconds').toDate();
-      return {
-        simulation: parseInt(d['Simulation #']),
-        time: t,
-        lat: parseFloat(d['Latitude']),
-        lon: parseFloat(d['Longitude']),
-        altitude: parseFloat(d['Altitude [m]'])
-      }
+    var t = startTime.clone().add(parseInt(d['Time from launch [s]']),'seconds').toDate();
+    return {
+      simulation: parseInt(d['Simulation #']),
+      time: t,
+      lat: parseFloat(d['Latitude']),
+      lon: parseFloat(d['Longitude']),
+      altitude: parseFloat(d['Altitude [m]'])
+    }
+  })
+  .get(function(error, rows){
+    chart.simulationData = rows;
+    chart.init();
+  })
+
+setInterval(function(){
+  d3.json('http://localhost:5000/aprs', function(heightData) {
+    heightData = _.map(heightData, function(d){
+      d.time = new Date(d.time*1000);
+      return d;
     })
-    .get(function(error, rows){
+    chart.updateHeight(heightData);
 
-      chart.simulationData = rows;
-      chart.init();
-    })
-
-});
+  });
+}, 1000);
--- app.js
+++ app.js
@@ -2,6 +2,14 @@
 var express = require('express');
 var redis = require('redis');
 var url = require('url');
+var moment = require('moment');
+var request = require('request');
+var _ = require('underscore');
+
+
+//var aprsName = 'OZ2CLJ-11';
+var aprsName = 'K6RPT-11';
+
 
 var redisURL = url.parse(process.env.REDIS_URL);
 var client = redis.createClient(redisURL.port, redisURL.hostname);
@@ -32,14 +40,74 @@ app.get('/', function(request, response) {
 });
 
 app.get('/startup', function(request, response) {
-
   client.get('startup', function(err,d){
     response.send(d);
   })
+});
 
+app.get('/aprs', function(request, response) {
+  client.get('aprsPath', function(err,d){
+    response.send(d);
+  })
 });
 
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+var fetchAprs = function() {
+
+  var options = {
+    url: 'http://api.aprs.fi/api/get?apikey=78629.UvwKIyJM4Mz3W&name='+aprsName+'&format=json&what=loc',
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      console.log(info);
+      if(info.entries){
+
+        client.get('aprsPath', function(err,d){
+          var path= [];
+          if(d){
+            path = JSON.parse(d);
+          }
+
+          console.log(info);
+          var lastTimeUnix = parseInt(info.entries[0].lasttime);
+          var lastTime = moment.unix(lastTimeUnix);
+
+          if(path.length == 0 || _.last(path).time != lastTimeUnix){
+            path.push({
+              time: lastTimeUnix,
+              lat: parseFloat(info.entries[0].lat),
+              lng: parseFloat(info.entries[0].lng),
+              altitude: parseFloat(info.entries[0].altitude),
+              speed: parseFloat(info.entries[0].speed),
+              name: info.entries[0].name
+            })
+
+            client.set('aprsPath',JSON.stringify(path));
+          }
+
+        })
+
+
+
+
+
+      }
+     // console.log(info);
+
+    }
+  }
+
+  request(options, callback);
+}
+
+fetchAprs();
+setInterval(fetchAprs, 60000);
